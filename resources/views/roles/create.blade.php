b5b93230c53dfca2644d96c8e5ca5207d4df984e
Henry Ybalez <henryybalez33@gmail.com>
2023-08-05 14:28:25 +0800

    Update

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.master')
 @section('content')
 <div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Create New Role</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('roles.index') }}"> Back</a>
-        </div>
+    <div class="col-lg-6">
+        <h2>Create New Role</h2>
+    </div>
+    <div class="col-lg-6 text-right">
+        <a class="btn btn-primary" href="{{ route('roles.index') }}"> Back</a>
     </div>
 </div>
 @if (count($errors) > 0)
@@ -22,27 +20,28 @@
 @endif
 {!! Form::open(array('route' => 'roles.store','method'=>'POST')) !!}
 <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
+    <div class="col-md-6">
         <div class="form-group">
             <strong>Name:</strong>
             {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
         </div>
     </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
+    <div class="col-md-6">
         <div class="form-group">
             <strong>Permission:</strong>
             <br/>
             @foreach($permission as $value)
                 <label>{{ Form::checkbox('permission[]', $value->id, false, array('class' => 'name')) }}
                 {{ $value->name }}</label>
-            <br/>
+                <br/>
             @endforeach
         </div>
     </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+</div>
+<div class="row">
+    <div class="col-md-12 text-center">
         <button type="submit" class="btn btn-primary">Submit</button>
     </div>
 </div>
 {!! Form::close() !!}
-<p class="text-center text-primary"><small>Tutorial by LaravelTuts.com</small></p>
 @endsection
